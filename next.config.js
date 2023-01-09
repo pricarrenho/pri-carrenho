01d861d3277d6e8981f767f109e7527bd6dc9b19
Priscilla Carrenho <priscillaonil@gmail.com>
2023-01-09 18:24:47 -0300

    chore: add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    domains: ["media.graphassets.com"],
+  },
+});
