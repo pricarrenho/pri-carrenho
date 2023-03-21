4ba9565947787aaf1bcc27f23149b7453eb2bce0
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-21 19:25:53 -0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,20 @@
 import { HeaderProps } from "./types";
 import * as S from "./styles";
-import Container from "../Container";
 import Typewriter from "typewriter-effect";
 
 const Header = ({ title, subtitle }: HeaderProps) => {
   return (
-    <Container>
-      <S.Wrapper>
-        <S.Title dangerouslySetInnerHTML={{ __html: title }} />
-        <S.Subtitle>
-          <Typewriter
-            options={{ loop: true }}
-            onInit={(typewriter) => {
-              typewriter.typeString(subtitle).pauseFor(2500).start();
-            }}
-          />
-        </S.Subtitle>
-      </S.Wrapper>
-    </Container>
+    <S.Wrapper>
+      <S.Title dangerouslySetInnerHTML={{ __html: title }} />
+      <S.Subtitle>
+        <Typewriter
+          options={{ loop: true }}
+          onInit={(typewriter) => {
+            typewriter.typeString(subtitle).pauseFor(2500).start();
+          }}
+        />
+      </S.Subtitle>
+    </S.Wrapper>
   );
 };
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,14 +5,10 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 80px;
-  color: white;
 `;
 
 export const Title = styled.h1`
   ${({ theme }) => css`
-    color: ${theme.colors.white};
-    font-size: ${theme.font.sizes.xxl};
-
     strong {
       color: ${theme.colors.pink[700]};
     }
@@ -21,9 +17,9 @@ export const Title = styled.h1`
 
 export const Subtitle = styled.div`
   ${({ theme }) => css`
-    color: ${theme.colors.white};
     font-size: ${theme.font.sizes.md};
     position: relative;
+    min-height: 32px;
 
     strong {
       color: ${theme.colors.pink[700]};
