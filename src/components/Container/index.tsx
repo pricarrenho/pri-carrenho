a30519ff8e398f89bc38b6dd0b408a5f6e8842f2
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-21 19:25:30 -0300

    feat: add container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,8 +1,8 @@
 import { ContainerProps } from "./types";
 import * as S from "./styles";
 
-function Container({ children, ...props }: ContainerProps) {
+const Container = ({ children, ...props }: ContainerProps) => {
   return <S.Wrapper {...props}>{children}</S.Wrapper>;
-}
+};
 
 export default Container;
