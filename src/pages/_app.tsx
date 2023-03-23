e0b33dbc6334f2f00be9b1bbbad5eef7370ef950
Priscilla Carrenho <priscillaonil@gmail.com>
2023-03-23 13:56:35 -0300

    docs: change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { GlobalStyle } from "@/styles/globals";
 import { themeBlack } from "@/styles/theme";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={themeBlack}>
+      <Head>
+        <title>Priscilla Carrenho</title>
+        <meta name="theme-color" content="#13131a" />
+        <meta
+          name="description"
+          content="Priscilla Carrenho desenvolvedora Front End - Site portfólio"
+        />
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
