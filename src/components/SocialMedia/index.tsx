87ba3da9ceb27a7508be4649154398ea158f210a
Priscilla <priscillaonil@gmail.com>
2024-06-18 18:12:25 -0300

    feat: add aria label

--- src/components/SocialMedia/index.tsx
+++ src/components/SocialMedia/index.tsx
@@ -7,18 +7,27 @@ import * as S from "./styles";
 const SocialMedia = () => {
   return (
     <S.Wrapper>
-      <S.ImgLink href="https://github.com/pricarrenho" target="_blank">
+      <S.ImgLink
+        href="https://github.com/pricarrenho"
+        target="_blank"
+        aria-label="Confira meu github"
+      >
         <FaGithub size={24} />
       </S.ImgLink>
 
       <S.ImgLink
         href="https://www.linkedin.com/in/priscilla-carrenho/"
         target="_blank"
+        aria-label="Confira meu linkedin"
       >
         <FaLinkedinIn size={24} />
       </S.ImgLink>
 
-      <S.ImgLink href="mailto:priscillaonil@gmail.com" target="_blank">
+      <S.ImgLink
+        href="mailto:priscillaonil@gmail.com"
+        target="_blank"
+        aria-label="Entre em contato pelo email"
+      >
         <IoMdMail size={26} />
       </S.ImgLink>
     </S.Wrapper>
